0f1fb10e175c4ebd84a93aa800b354a1f6d9cd0e
PscheidtLucas <163616249+PscheidtLucas@users.noreply.github.com>
2025-05-28 19:22:58 -0300

    Atualização da weapon e bullet

--- weapon/bullet.gd
+++ weapon/bullet.gd
@@ -1,14 +1,15 @@
 extends Area2D
 
-var speed: int = 500
-var max_range: float = 1000.0
+var speed: int = 500 #sobreescrito no script da arma
+var max_range: float = 1000.0 #sobreescrito no script da arma
 
 var _traveled_distance: float = 0.0
-var _direction: Vector2 = Vector2(1, 0).rotated(rotation)
 
 func _physics_process(delta: float) -> void:
 	var distance:= speed * delta
-	position += distance * _direction 
+	var motion := Vector2.RIGHT.rotated(rotation) * distance
+	
+	position += motion
 
 	_traveled_distance += distance
 	if _traveled_distance >= max_range:
@@ -16,5 +17,5 @@ func _physics_process(delta: float) -> void:
 		
 func _destroy() -> void:
 	var fade_out_tween := create_tween()
-	fade_out_tween.tween_property(self, "modulate:a", 0, 0.3)
+	fade_out_tween.tween_property(self, "modulate:a", 0, 0.2)
 	fade_out_tween.tween_callback(queue_free)
---
+++ weapon/weapon.gd
@@ -0,0 +1,27 @@
+class_name Weapon extends Node2D
+
+@export var bullet_scene: PackedScene
+
+# Ângulo de spread da arma, quanto maior, mais imprecisa é a arma
+@export_range(0.0, 360.0, 1.0, "radians_as_degrees") var random_angle := PI / 12.0
+
+@export_range(100.0, 2000.0, 1.0) var max_range := 200.0
+@export_range(100.0, 3000.0, 1.0) var max_bullet_speed := 1500.0
+@onready var marker_2d: Marker2D = $Marker2D
+
+
+func _physics_process(_delta: float) -> void:
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+
+
+# Faz a arma atirar uma vez, essa função é sobrescrita quando precisa de um comportamente diferente
+func shoot() -> void:
+	var bullet: Node = bullet_scene.instantiate()
+	get_tree().current_scene.add_child(bullet)
+
+	bullet.global_position = marker_2d.global_position
+	bullet.global_rotation = marker_2d.global_rotation
+	bullet.max_range = max_range
+	bullet.speed = max_bullet_speed
+	bullet.rotation += randf_range(-random_angle / 2.0, random_angle / 2.0)
